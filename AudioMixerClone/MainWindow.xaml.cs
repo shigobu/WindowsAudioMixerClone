35c17f249986b679480e2d9d459e0455bf5096b2
kasamatsu <kasamatsu@kasa.com>
2022-02-22 15:22:41 +0900

    ミュートに関するプロパティ追加

--- AudioMixerClone/MainWindow.xaml.cs
+++ AudioMixerClone/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Windows;
 using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace AudioMixerClone
 {
@@ -12,12 +13,18 @@ namespace AudioMixerClone
     /// </summary>
     public partial class MainWindow : Window
     {
+        // タイマのインスタンス
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
 
-
-            System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcesses();
+            // タイマのインスタンスを生成
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += _timer_Tick;
+            timer.Start();
 
             using (MMDeviceEnumerator DevEnum = new MMDeviceEnumerator())
             using (MMDevice device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
@@ -25,6 +32,7 @@ namespace AudioMixerClone
                 //マスターの設定
                 masterSlider.displayNameTextBlock.Text = "マスター";
                 masterSlider.slider.Value = device.AudioEndpointVolume.MasterVolumeLevelScalar * 100;
+                masterSlider.IsMute = device.AudioEndpointVolume.Mute;
                 using(Icon icon = GetIconFromIconPath(device.IconPath))
                 {
                     if (icon != null)
@@ -35,52 +43,80 @@ namespace AudioMixerClone
 
                 //各アプリの追加
                 SessionCollection sessionCollection = device.AudioSessionManager.Sessions;
-                for (int i = 0; i < sessionCollection.Count; i++)
-                {
-                    AudioSessionControl audioSession = sessionCollection[i];
-                    SliderAndIcon sliderAndIcon = new SliderAndIcon();
-                    sliderAndIcon.SessionIndex = i;
-                    sliderAndIcon.SliderValueChanged += Slider_ValueChanged;
-
-                    if (audioSession.IsSystemSoundsSession)
-                    {
-                        sliderAndIcon.displayNameTextBlock.Text = "システム音";
-                        string iconpath = audioSession.IconPath;
-                        using (Icon icon = GetIconFromIconPath(iconpath))
-                        {
-                            if (icon == null)
-                            {
-                                continue;
-                            }
-                            sliderAndIcon.mainImage.Source = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, null);
-                        }
-                        sliderAndIcon.slider.Value = audioSession.SimpleAudioVolume.Volume * 100;
-                        appStackPanel.Children.Insert(0, sliderAndIcon); //システム音声は、アプリの最初に追加
-                    }
-                    else
-                    {
-                        string displayName = audioSession.DisplayName;
-                        if (string.IsNullOrWhiteSpace(displayName))
-                        {
-                            displayName = ps.First(x => x.Id == audioSession.GetProcessID).MainWindowTitle;
-                            if (string.IsNullOrWhiteSpace(displayName))
-                            {
-                                displayName = ps.First(x => x.Id == audioSession.GetProcessID).ProcessName;
-                            }
-                        }
-                        sliderAndIcon.displayNameTextBlock.Text = displayName;
-
-                        string exeName = ps.First(x => x.Id == audioSession.GetProcessID).MainModule.FileName;
-                        using (Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(exeName))
-                        {
-                            sliderAndIcon.mainImage.Source = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, null);
-                        }
-                        sliderAndIcon.slider.Value = audioSession.SimpleAudioVolume.Volume * 100;
-                        appStackPanel.Children.Add(sliderAndIcon);
-                    }
-                }
+                UpdateAppStackPanel(sessionCollection);
             }
 
+        }
+
+        private void UpdateAppStackPanel(SessionCollection sessionCollection)
+        {
+            //一度Sliderを削除
+            appStackPanel.Children.Clear();
+
+            System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcesses();
+
+            for (int i = 0; i < sessionCollection.Count; i++)
+            {
+                AudioSessionControl audioSession = sessionCollection[i];
+                SliderAndIcon sliderAndIcon = new SliderAndIcon();
+                sliderAndIcon.SessionIndex = i;
+                sliderAndIcon.SliderValueChanged += Slider_ValueChanged;
+                sliderAndIcon.IsMute = audioSession.SimpleAudioVolume.Mute;
+
+                if (audioSession.IsSystemSoundsSession)
+                {
+                    sliderAndIcon.displayNameTextBlock.Text = "システム音";
+                    string iconpath = audioSession.IconPath;
+                    using (Icon icon = GetIconFromIconPath(iconpath))
+                    {
+                        if (icon == null)
+                        {
+                            continue;
+                        }
+                        sliderAndIcon.mainImage.Source = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, null);
+                    }
+                    sliderAndIcon.slider.Value = audioSession.SimpleAudioVolume.Volume * 100;
+                    appStackPanel.Children.Insert(0, sliderAndIcon); //システム音声は、アプリの最初に追加
+                }
+                else
+                {
+                    string displayName = audioSession.DisplayName;
+                    if (string.IsNullOrWhiteSpace(displayName))
+                    {
+                        displayName = ps.First(x => x.Id == audioSession.GetProcessID).MainWindowTitle;
+                        if (string.IsNullOrWhiteSpace(displayName))
+                        {
+                            displayName = ps.First(x => x.Id == audioSession.GetProcessID).ProcessName;
+                        }
+                    }
+                    sliderAndIcon.displayNameTextBlock.Text = displayName;
+
+                    string exeName = ps.First(x => x.Id == audioSession.GetProcessID).MainModule.FileName;
+                    using (Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(exeName))
+                    {
+                        sliderAndIcon.mainImage.Source = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, null);
+                    }
+                    sliderAndIcon.slider.Value = audioSession.SimpleAudioVolume.Volume * 100;
+                    appStackPanel.Children.Add(sliderAndIcon);
+                }
+            }
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            //オーディオセッションを監視して、変更が有った場合に画面の更新
+            using (MMDeviceEnumerator DevEnum = new MMDeviceEnumerator())
+            using (MMDevice device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
+            {
+                SessionCollection sessionCollection = device.AudioSessionManager.Sessions;
+                if (sessionCollection.Count != appStackPanel.Children.Count)
+                {
+                    UpdateAppStackPanel(sessionCollection);
+                }
+
+                //各オーディオセッションの音量やミュート状態を更新
+
+            }
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
--- AudioMixerClone/SliderAndIcon.xaml.cs
+++ AudioMixerClone/SliderAndIcon.xaml.cs
@@ -15,7 +15,34 @@ namespace AudioMixerClone
             slider.ValueChanged += (sender, e) => { SliderValueChanged?.Invoke(this, e); };
         }
 
+        private static string muteChar = "🔇";
+        private static string notMuteChar = "🔊";
+
         public int SessionIndex { get; set; }
+
+        public string ButtonText
+        {
+            get
+            {
+                return buttonTextBlock.Text;
+            }
+            set
+            {
+                buttonTextBlock.Text = value;
+            }
+        }
+
+        public bool IsMute
+        {
+            get
+            {
+                return ButtonText == muteChar;
+            }
+            set
+            {
+                ButtonText = value ? muteChar : notMuteChar;
+            }
+        }
 
         public event RoutedPropertyChangedEventHandler<double> SliderValueChanged;
     }
